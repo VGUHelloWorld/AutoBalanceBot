7f91a005ca17ac913c0fb81f9bf5299ce3d46949
nkquynh98 <37395583+nkquynh98@users.noreply.github.com>
2018-04-01 19:16:16 +0700

    Just for goto another branch

---
+++ Debug/UART/subdir_vars.mk
@@ -0,0 +1,44 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../UART/UART.c \
+../UART/ftoa.c \
+../UART/uartstdio.c \
+../UART/ustdlib.c 
+
+C_DEPS += \
+./UART/UART.d \
+./UART/ftoa.d \
+./UART/uartstdio.d \
+./UART/ustdlib.d 
+
+OBJS += \
+./UART/UART.obj \
+./UART/ftoa.obj \
+./UART/uartstdio.obj \
+./UART/ustdlib.obj 
+
+OBJS__QUOTED += \
+"UART\UART.obj" \
+"UART\ftoa.obj" \
+"UART\uartstdio.obj" \
+"UART\ustdlib.obj" 
+
+C_DEPS__QUOTED += \
+"UART\UART.d" \
+"UART\ftoa.d" \
+"UART\uartstdio.d" \
+"UART\ustdlib.d" 
+
+C_SRCS__QUOTED += \
+"../UART/UART.c" \
+"../UART/ftoa.c" \
+"../UART/uartstdio.c" \
+"../UART/ustdlib.c" 
+
+
